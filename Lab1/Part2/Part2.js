18f2f3e767651fa568416b172daf9a36d50212c6
Zeinab Abdelghaffar <zeinanabdo88@users.noreply.github.com>
2024-02-25 15:03:18 +0200

    Add Lab1 (Part1 and Part2) files

---
+++ Lab1/Part2/Part2.js
@@ -0,0 +1,14 @@
+const redRange = document.getElementById('redRange');
+const greenRange = document.getElementById('greenRange');
+const blueRange = document.getElementById('blueRange');
+const text = document.getElementById('text');
+function updateColor() {
+    const red = redRange.value;
+    const green = greenRange.value;
+    const blue = blueRange.value;
+    const color = `rgb(${red}, ${green}, ${blue})`;
+    text.style.color = color;
+}
+redRange.addEventListener('input', updateColor);
+greenRange.addEventListener('input', updateColor);
+blueRange.addEventListener('input', updateColor);x
