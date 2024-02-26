ff17cc1dfbcb89887ad1b10139337ccf2377c0de
Zeinab Abdelghaffar <zeinanabdo88@users.noreply.github.com>
2024-02-26 15:32:47 +0200

    Added Lab2 project files for Part1, Part2, Part3, and Part4

---
+++ Lab2/Part4/Part4.js
@@ -0,0 +1,27 @@
+var canvas = document.getElementById('myCanvas');
+var ctx = canvas.getContext('2d');
+var x = 0;
+var y = 0;
+var dx = 1;
+var dy = 1;
+
+function drawLine() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.beginPath();
+    ctx.moveTo(0, 0);
+    ctx.lineTo(x, y);
+    ctx.strokeStyle = 'red';
+    ctx.lineWidth = 3;
+    ctx.stroke();
+    x += dx;
+    y += dy;
+    if (x >= canvas.width || y >= canvas.height) {
+        x = 0;
+        y = 0;
+        alert('Animation end!');
+    } else {
+        requestAnimationFrame(drawLine);
+    }
+}
+
+drawLine();
